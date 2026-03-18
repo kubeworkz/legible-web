eea0b123a2cdf40fdf8aafb582c37d996e3709e4
Legible <dave@gridworkz.com>
2026-03-18 20:55:33 +0000

    feature: add new domain name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['legible.kubeworkz.io'],
+    allowedHosts: ['legiblequery.ai'],
   },
   server: {
     host: '0.0.0.0',
